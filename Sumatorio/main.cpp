1f8fcf321ac5d6ca680b58b6806a498d3f764077
cbotella <carlosb.n@icloud.com>
2021-09-23 12:07:16 +0200

    Initial Commit

---
+++ Sumatorio/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Sumatorio
+//
+//  Created by Carlos Botella Navarro on 23/9/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
