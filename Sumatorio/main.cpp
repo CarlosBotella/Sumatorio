9781dcf278bb83ecd8cd2564d4611f7f70cd3bca
cbotella <carlosb.n@icloud.com>
2021-10-07 11:18:53 +0200

    Sumatorio 1.0

--- Sumatorio/main.cpp
+++ Sumatorio/main.cpp
@@ -5,20 +5,28 @@
 //  Created by Carlos Botella Navarro on 23/9/21.
 //
 
+/*
+ ------------------------------------------------
+ Datos de entrada: n : N
+ Datos de salida: N
+ Devolver (n·(n+1))/2
+ ------------------------------------------------
+ */
 #include <iostream>
 using namespace std;
 
-void sumatorio(){
-    int n;
-    int total = 0;
-    cout << "Elige un número";
-    cin >> n;
-    for (int i = 0; i <= n; i++) {
-        total = total + i;
+unsigned sumatorio(unsigned n){
+    unsigned int total = 0;
+    for (int numeroAsumar = 0; numeroAsumar <= n; numeroAsumar++) {
+        total = total + numeroAsumar;
     }
     cout << "resultado" << total;
+    return total;
 }
 
 int main() {
-    sumatorio();
+    int numero;
+    cout << "Elige un número";
+    cin >> numero;
+    sumatorio(numero);
 }
