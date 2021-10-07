712ece5d491e509bc80529dbba37e338dfe79e59
cbotella <carlosb.n@icloud.com>
2021-10-07 15:26:40 +0200

    Sumatorio 1.0

--- Sumatorio/main.cpp
+++ Sumatorio/main.cpp
@@ -14,19 +14,19 @@
  */
 #include <iostream>
 using namespace std;
-
+// ----------------------------------------------
 unsigned sumatorio(unsigned n){
     unsigned int total = 0;
     for (int numeroAsumar = 0; numeroAsumar <= n; numeroAsumar++) {
         total = total + numeroAsumar;
     }
-    cout << "resultado" << total;
     return total;
 }
-
+// ----------------------------------------------
 int main() {
-    int numero;
-    cout << "Elige un número";
-    cin >> numero;
-    sumatorio(numero);
+    int r;
+    r = sumatorio(4);
+    if (r != 10) {
+        cout << "Hay un error";
+    } else { cout << "No hay ningún error" << endl;}
 }
